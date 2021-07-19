6920b82ec010a3b296246a6f38f4d41f728db15b
William Walker <w_walker@icloud.com>
2021-07-19 14:12:40 -0400

    Initial Commit

---
+++ Shared/mcheyne_planApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcheyne_planApp.swift
+//  Shared
+//
+//  Created by Will Walker on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct mcheyne_planApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
