24513e8c9f340df44471aea71208cc2fff582914
William Walker <w_walker@icloud.com>
2021-09-17 12:04:52 -0500

    add support for macOS

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -23,6 +23,7 @@ struct ContentView: View {
                     SettingsView()
                 })
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     func toggleSettingsView() {
